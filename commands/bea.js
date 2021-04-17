6cba3a9d9d1649e9a3a6cd37bc12d7f0eaa3fe90
sarahanduca <anduca.sarah@gmail.com>
2021-04-17 17:14:21 -0300

    add portuguese

--- commands/bea.js
+++ commands/bea.js
@@ -2,7 +2,7 @@ import { MessageEmbed } from "discord.js";
 let url = "https://www.wattpad.com/story/266206201-livro-sem-nome";
 export default function (msg) {
   const embed = new MessageEmbed()
-    .setDescription(`Le o livrinho da Bea 💖 \n${url}`)
+    .setDescription(`Lê o livrinho da Bea 💖 \n${url}`)
     .setColor(0xff69b4);
   msg.channel.send(embed);
 }
